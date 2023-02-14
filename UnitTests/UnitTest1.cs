a6a81c3950dde7bdd52acfd4374eda7468dfd8eb
hhhEEElll <rtuezptim@mail.ru>
2023-02-15 00:42:47 +0300

    Добавьте файлы проекта.

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using test;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // Проверка, что свойство которое хранит размер шрифта имеет ограничения на ввод слишком большого числа (не влезающий в int)
+        [TestMethod]
+        public void GetFontSize_BigData()
+        {
+            long expected = 999999999999999999;
+            var tar = new Start() { s = (int)expected };
+            Assert.AreEqual(expected, tar.s);
+        }
+        // Проверка на корректное значение
+        [TestMethod]
+        public void GetFontSize_Correct()
+        {
+            int expected = 3;
+            var tar = new Start() { s = expected };
+            Assert.AreEqual(expected, tar.s);
+        }
+        // Проверка, что свойство которое хранит размер шрифта имеет ограничения на ввод слишком маленького числа (не влезающий в int)
+        [TestMethod]
+        public void GetFontSize_SmallData()
+        {
+            long expected = -999999999999999999;
+            var tar = new Start() { s = (int)expected };
+            Assert.AreEqual(expected, tar.s);
+        }
+        // 
+    }
+}
